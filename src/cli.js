bc6214316729b9fd1883f67eeb3046f0511bea9f
Aria Taylor <ari@aricodes.net>
2020-05-29 18:47:43 -0400

    Different import

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
+import path from 'path';
+
 // This is just a simple wrapper to avoid having the shell bang in the module
-import run from './index';
+import run from path.join(__dirname, 'index');
 
 run().catch(e => {throw new Error(e)});
